e85ed0ab5739441babcc82f4783b106c91024ae8
RkNuViPRaSyS <34408593+RkNuViPRaSyS@users.noreply.github.com>
2025-07-01 17:46:18 +0100

    Add files via upload

--- src/spectrasight/app.py
+++ src/spectrasight/app.py
@@ -1,1 +1,10 @@
+from toga import App, MainWindow, Label
 
+def build(app):
+    main_window = MainWindow(title=app.formal_name)
+    main_window.content = Label("SpectraSight is ready.")
+    main_window.show()
+    return main_window
+
+def main():
+    return App("SpectraSight", "com.spectrasight", startup=build)
